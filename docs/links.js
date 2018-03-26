71ae2d21c5ab45196de264cc7f9a0fae01a9866a
Dylan <>
2018-03-26 16:32:31 +1100

    fix

--- docs/links.js
+++ docs/links.js
@@ -15,9 +15,9 @@ function loadjscssfile(filename, filetype){
 }
  
 loadjscssfile("https://cdnjs.cloudflare.com/ajax/libs/bulma/0.5.3/css/bulma.min.css", "css") ////dynamically load and add this .css file
-//loadjscssfile("loading.css", "css")
+loadjscssfile("loading.css", "css")
 
 loadjscssfile("https://use.fontawesome.com/releases/v5.0.6/js/all.js", "js")
-//loadjscssfile("loading.js", "js")
+loadjscssfile("loading.js", "js")
 loadjscssfile("https://www.w3schools.com/lib/w3.js", "js")
 
